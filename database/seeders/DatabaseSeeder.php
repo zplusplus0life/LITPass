2a44b342a56f144312786b2583be7fe7590d0dba
zcky <255608195+zplusplus0life@users.noreply.github.com>
2026-03-23 13:14:58 +0700

    feat(Data-Seed): add 2 new models, add db::statement() and add truncate() for db seed issue about data duplicate

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,11 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Plan;
+use App\Models\Book;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,7 +16,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        
+
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+
+        Book::truncate();
+        Plan::truncate();
+        User::truncate();
+
         $this->call(PlanSeeder::class);
 
         \App\Models\Book::factory(20)->create();
@@ -22,5 +31,7 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        DB::statement('SET FOREIGN_KEY_CHECKS=1');
     }
 }
