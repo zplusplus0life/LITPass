b85604b98176559cefa7724e21f110f23cd8f6e6
zcky <255608195+zplusplus0life@users.noreply.github.com>
2026-03-23 16:38:18 +0700

    feat(api): route root, and route get /books

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Laravel\Fortify\Features;
+use App\Http\Controllers\BookController;
+
 
 Route::get('/', function () {
-    return Inertia::render('welcome', [
-        'canRegister' => Features::enabled(Features::registration()),
-    ]);
+    return redirect()->route('books.indeks');
 });
+
+Route::get('/books', [BookController::class, 'index'])->name('books.indeks');
