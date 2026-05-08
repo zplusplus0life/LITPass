bc75734f1bffd7166785fff0ac7952c5caa43def
zcky <255608195+zplusplus0life@users.noreply.github.com>
2026-05-08 11:19:21 +0700

    feat: update authlayout for admin

--- resources/js/layouts/authLayout.tsx
+++ resources/js/layouts/authLayout.tsx
@@ -3,6 +3,10 @@ import {useState} from 'react';
 import Dropdown from '@/components/Dropdown';
 import * as Route from '@/routes/index';
 import {index as planIndex} from '@/routes/plans';
+import {index as AdminUsers} from '@/routes/admin/users';
+import {index as AdminPlans} from '@/routes/admin/plans';
+import {index as AdminBooks} from '@/routes/admin/books';
+import {dashboard as adminDashboard} from '@/routes/admin';
 // import {edit as profileEdit} from '@/routes/profile';
 import {indeks as bookIndex} from '@/routes/books';
 import NavLink from '@/components/NavLink';
@@ -12,6 +16,15 @@ export default function AuthLayout({header, children}: any){
     const route = usePage().props.route as string;
     const [showData, setData] = useState(false);
 
+    const currentRouteIs = (target: string) => {
+
+        if (target.endsWith('.*')){
+            const change = target.replace('.*', '');
+            return target.startsWith(change);
+        }
+        return route === target;
+    }
+
     return (
         <div className="min-h-screen bg-gray-100">
             <nav className="border-b border-gray-100 bg-white">
@@ -19,27 +32,58 @@ export default function AuthLayout({header, children}: any){
                     <div className="flex h-16 justify-between">
 
                         <div className="hidde space-x-8 sm:ms-10 sm:flex">
-                            
-                            {user && (
-                                <NavLink 
-                                href={Route.dashboard().url}
-                                active={route === 'dashboard'}>
-                                    Dashboard
-                                </NavLink>
-                            )}
-                            <NavLink 
-                            href={bookIndex().url}
-                            active={route.startsWith('book.*')}>
-                                Books Catalog
-                            </NavLink>
+                              {user && user.role === 'admin' ? (
+                                    <>
+                                        <NavLink
+                                            href={adminDashboard().url}
+                                            active={currentRouteIs(route)}
+                                        >
+                                            Admin Dashboard
+                                        </NavLink>
+                                           <NavLink
+                                            href={AdminUsers().url}
+                                            active={currentRouteIs(route)}
+                                        >
+                                            Manage Users
+                                        </NavLink>
+                                        <NavLink
+                                            href={AdminPlans().url}
+                                            active={currentRouteIs(route)}
+                                        >
+                                            Manage Plans
+                                        </NavLink>
+                                        <NavLink
+                                            href={AdminBooks().url}
+                                            active={currentRouteIs(route)}
+                                        >
+                                            Manage Books
+                                        </NavLink>
+                                    </>
+                              ) : (
+                                <>
+                                   {user && (
+                                         <NavLink 
+                                              href={Route.dashboard().url}
+                                              active={currentRouteIs(route)}>
+                                             Dashboard
+                                        </NavLink>
+                                    )}
+                                        <NavLink 
+                                        href={bookIndex().url}
+                                        active={currentRouteIs(route)}>
+                                            Books Catalog
+                                        </NavLink>
 
 
-                            <NavLink 
-                            href={planIndex().url}
-                            active={route === 'plans.index'}>
-                                Membership Plans
-                            </NavLink>
-                        </div>
+                                        <NavLink 
+                                        href={planIndex().url}
+                                        active={currentRouteIs(route)}>
+                                            Membership Plans
+                                        </NavLink>
+                                </>
+                              )}
+                            </div>
+                         
                         <div className="hidden sm:ms-6 sm:flex sm:items-center">
                             <div className="ms-3">
                                 {user ? (
